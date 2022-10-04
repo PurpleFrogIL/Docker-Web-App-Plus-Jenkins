042389c72d2c13bc16782615097e32616e9abee3
Dror Felman <dror.f@outlook.com>
2022-10-04 10:23:26 +0300

    Add 'if __name__ ...' condition and reformat

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask("__main__")
 
 @app.route("/")
 def hello_world():
-        return "<p>Hello, World!</p>"
+	return "<p>Hello, World!</p>"
 
-app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=80)
