f285f3b5a55e2fb7b47702db93b42731d9588f85
PurpleFrogIL <dror.f@outlook.com>
2022-11-21 22:37:06 +0200

    Add bad test for testing pipeline

---
+++ test/bad_test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+echo "Starting test script..."
+echo "SHELL=$SHELL"
+
+URL="44.211.103.126"
+PORT=9999
+TIMEOUT=2
+
+
+for i in {1..10}
+do
+    echo "Try number ${i}"
+    RESULT=$(
+        curl --include --silent --connect-timeout $TIMEOUT $URL:$PORT |
+        head -n 1 |
+        cut -d ' ' -f 2
+    )
+
+    if [[ $RESULT -eq 200 ]]; then
+        echo "Got '200 OK'"
+        exit 0  # good
+    else
+        echo "Got other value than '200 OK'"
+        sleep 3
+    fi
+done
+
+exit 1
