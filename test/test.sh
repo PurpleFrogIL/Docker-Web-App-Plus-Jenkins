c822e3e17f16bddf5017608841b14137f7e3c4f5
PurpleFrogIL <dror.f@outlook.com>
2022-11-21 21:46:58 +0200

    Working

--- test/test.sh
+++ test/test.sh
@@ -5,6 +5,7 @@ echo "Starting test script..."
 URL="44.211.103.126"
 PORT=3000
 TIMEOUT=10
+EXIT_CODE=1  # bad
 
 RESULT=$(
 	curl --include --silent --connect-timeout $TIMEOUT $URL:$PORT |
@@ -12,12 +13,8 @@ RESULT=$(
 	cut -d ' ' -f 2
 )
 
-printf "!${RESULT}!"
-echo "new"
-echo "!${RESULT}!"
-
-if [ $RESULT -eq 200 ]; then
-	echo "Online"
-else
-	echo "Offline"
+if [[ $RESULT -eq 200 ]]; then
+	EXIT_CODE=0  # good
 fi
+
+exit $EXIT_CODE
