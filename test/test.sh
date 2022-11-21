51a016fc200975a5084d8a21928586a13d464436
PurpleFrogIL <dror.f@outlook.com>
2022-11-21 21:05:34 +0200

    Change to check only result number (200)

--- test/test.sh
+++ test/test.sh
@@ -2,13 +2,13 @@
 
 echo "Starting test script..."
 
-RESULT=$(curl --include --silent --connect-timeout 10 44.211.103.126:3000 | head -n 1 | cut -d ' ' -f 2,3)
+RESULT=$(curl --include --silent --connect-timeout 10 44.211.103.126:3000 | head -n 1 | cut -d ' ' -f 2)
 
 printf "!${RESULT}!"
 echo "new"
 echo "!${RESULT}!"
 
-if [[ "$RESULT" == "200 OK" ]]; then
+if [ $RESULT -eq 200 ]; then
 	echo "Online"
 else
 	echo "Offline"
